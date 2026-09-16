d8a6aa2e7d729ff5d837721f7225f6947f5da3b4
You-commit <285901716+You-commit@users.noreply.github.com>
2026-09-16 15:59:54 +0900

    refactor: keep NISA affiliate inside calculator flow

--- src/pages/NisaPage.tsx
+++ src/pages/NisaPage.tsx
@@ -1,6 +1,4 @@
 import NisaCalculator from '../NisaCalculator'
-import { affiliatePrograms } from '../affiliate/affiliateConfig'
-import AffiliateProgramPlacement from '../components/affiliate/AffiliateProgramPlacement'
 import SimulatorNotes from '../components/simulator/SimulatorNotes'
 import SimulatorPageShell from '../components/simulator/SimulatorPageShell'
 
@@ -29,10 +27,6 @@ function NisaPage() {
         />
       )}
     >
-      <AffiliateProgramPlacement
-        program={affiliatePrograms.nisa}
-        placement="nisa-before-simulation"
-      />
       <NisaCalculator />
     </SimulatorPageShell>
   )
